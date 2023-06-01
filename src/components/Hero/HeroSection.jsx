3e12367dafc8a193bb97a07701f7b218bfb25f7c
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-06-01 11:02:09 -0500

    add hero components and imgs

---
+++ src/components/Hero/HeroSection.jsx
@@ -0,0 +1,19 @@
+import ImgGallery from "./ImgGallery";
+
+const HeroSection = () => {
+  return (
+    <section className="w-1/2">
+      <div>
+        <h1 className=" text-title text-[40px] font-bold">La manera mas fácil de comprar</h1>
+        <p className="text-[30px]">
+          Regístrate, busca y obtenlo. Encuentra el producto que desees a
+          precios inimaginables, con ofertas que ayudaran a crecer tus bolsillos
+          y con simples maneras de pago.
+        </p>
+      </div>
+      <ImgGallery />
+    </section>
+  );
+};
+
+export default HeroSection;
