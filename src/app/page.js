f2d24add0e6b36c04c3070ed2cc89b1fb5a32478
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-06-01 12:37:29 -0500

    add homepage desktop

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,20 @@
+import FooterSec from "@/components/Footer/FooterSec";
 import Navbar from "@/components/Header/Navbar";
 import HeroSection from "@/components/Hero/HeroSection";
 import FormLogin from "@/components/Login/FormLogin";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen max-w-[1200px] mx-auto">
+    <>
+    <main className=" min-h-[calc(100vh-40px)] max-w-[1200px] mx-auto">
       <Navbar />
-      <div className="flex">
+      <div className="flex gap-16">
         <HeroSection />
         <FormLogin />
       </div>
+      
     </main>
+    <FooterSec/>
+    </>
   );
 }
