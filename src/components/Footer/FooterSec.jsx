586c460c886b96f3eae12cc2a0ef083aef826b94
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-06-07 11:42:08 -0500

    add maps, indicador corrections styles

--- src/components/Footer/FooterSec.jsx
+++ src/components/Footer/FooterSec.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FooterSec = () => {
   return (
-    <p className="hidden md:block text-[23px] text-title text-center mb-2">
+    <p className="hidden md:block text-[20px] text-title text-center mb-2 mt-5">
       Takiven 2023 - <Link href="/" className=" font-[500]">Términos y condiciones</Link> -{" "}
       <Link href="/" className=" font-[500]">Privacidad</Link> - <Link href="/" className=" font-[500]">Legal</Link> {" "}
     </p>
