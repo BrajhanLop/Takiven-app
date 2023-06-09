c8751ea364a703e27a8be1dcc21fb5b2565fcb18
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-06-09 12:52:02 -0500

    add cards cart, cuenta

---
+++ src/components/Dashboard/CuentMenu.jsx
@@ -0,0 +1,29 @@
+import puser from "../../assets/img/icons/puser.png";
+import extend from "../../assets/img/icons/extend.png";
+import obs from "../../assets/img/icons/obs.png";
+import Image from "next/image";
+const CuentMenu = () => {
+  return (
+    <div className=" bg-[#3FD077] absolute top-40 right-44 rounded rounded-b-[20px] w-[228px] h-[160px] text-white">
+      <div>
+        <p className=" text-[20px] font-[500] text-center bg-[#30F27B] py-4">
+          Hola, Paulo Abad
+        </p>
+      </div>
+      <div className=" h-[98px] ">
+        <div className="flex justify-center items-center gap-4 text-[20px] h-1/2 font-[500]">
+          <Image src={puser} alt="user" />
+          <p className="w-[111px]">Mi Cuenta</p>
+          <Image src={extend} alt="link" />
+        </div>
+        <div className="flex justify-center items-center gap-4 text-[20px] h-1/2 font-[500]">
+          <Image src={obs} alt="user" />
+          <p className="w-[111px]">Mis Pedidos</p>
+          <Image src={extend} alt="link" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CuentMenu;
--- src/components/Dashboard/GamerSection.jsx
+++ src/components/Dashboard/GamerSection.jsx
@@ -1,13 +1,27 @@
+"use client";
 import Image from "next/image";
 import play from "../../assets/img/icons/play.png";
-
+import cohete from "../../assets/img/icons/cohete.png";
+import CardProduct from "../Product/CardProduct";
 const GamerSection = () => {
   return (
-    <div className="flex items-center gap-3 max-w-[1200px] mx-auto mt-10">
-      <Image src={play} className="w-[55px]" />
+    <div className="max-w-[1200px] mx-auto">
+      <div className="flex items-center gap-3 max-w-[1200px] mx-auto mt-10">
+        <Image src={play} className="w-[55px]" />
 
-      <p className="text-[#3FD077] text-[30px] font-[500]">Gamer</p>
-      <hr className="mt-5 border-[#3FD07766] w-[80%] mx-auto " />
+        <p className="text-[#3FD077] text-[30px] font-[500]">Gamer</p>
+        <hr className="mt-5 border-[#3FD07766] w-[80%] mx-auto " />
+        <Image src={cohete} className="w-[55px]" />
+        <p className="text-[#3FD077] text-[30px] font-[500]">Ver</p>
+        <p className="text-[#3FD077] text-[30px] font-[500]">más</p>
+      </div>
+
+      <div className="flex gap-4 mt-5">
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
     </div>
   );
 };
