137825c500080ee7b67bcd4db6a7ea3c29dce787
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-07-31 13:00:51 -0500

    ad responsive

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -12,9 +12,9 @@ const page = () => {
     <div className=" bg-[#EDF0F1]  flex">
       <div className="flex gap-1 max-w-[1350px]">
         <Sidebar />
-        <div className="w-full flex">
-          <div className="w-7/12">
-            <div className="flex justify-between flex-wrap gap-2 p-4 ">
+        <div className="w-full flex flex-col lg:flex-row">
+          <div className="lg:w-7/12">
+            <div className="flex justify-evenly lg:justify-between flex-wrap gap-2 p-4 ">
               <Card img={card1} title="Ventas del lunes" amount="S/. 1,000.00" />
               <Card img={card1} title="Ventas de la semana" amount="S/. 2,000.00" />
               <Card img={card1} title="Ventas total del mes" amount="S/. 8,000.00" />
@@ -25,10 +25,10 @@ const page = () => {
             <div className="bg-white rounded-[20px] p-4 mx-4 ">
               <div className="flex gap-3">
                 <Image src={despacho} alt="" />
-                <p className=" text-[28px] font-[400]">Despacho</p>
+                <p className=" xl:text-[28px] lg:text-[26px] font-[400]">Despacho</p>
               </div>
               <div className="">
-                <div className=" flex text-[18px] font-[400] pt-4 ">
+                <div className=" flex xl:text-[18px] lg:text-[15px] font-[400] pt-4 ">
                   <p className=" w-1/4 text-center">Repartidor</p>
                   <p className=" w-1/4 text-center">Distrito</p>
                   <p className=" w-1/4 text-center">Status</p>
@@ -36,7 +36,7 @@ const page = () => {
                   <p className=" w-1/4 text-center"></p>
                 </div>
                 <hr className=" border-1 border-black mb-3" />
-                <div className="flex py-2 text-[18px] mb-3  items-center">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px] mb-3  items-center">
                   <p className=" w-1/4 text-center">Paulo Abad</p>
                   <p className=" w-1/4 text-center">Jesus Maria</p>
                   <p className=" w-1/4 text-center">En camino</p>
@@ -45,8 +45,7 @@ const page = () => {
                     Detalle
                   </p>
                 </div>
-
-                <div className="flex py-2 text-[18px] mb-3  items-center">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px] mb-3  items-center">
                   <p className=" w-1/4 text-center">Takiven Perú</p>
                   <p className=" w-1/4 text-center">San Martin de Porres</p>
                   <p className=" w-1/4 text-center">Recibido</p>
@@ -55,7 +54,7 @@ const page = () => {
                     Detalle
                   </p>
                 </div>
-                <div className="flex py-2 text-[18px] mb-3 items-center">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px] mb-3 items-center">
                   <p className=" w-1/4 text-center">Juan Peña</p>
                   <p className=" w-1/4 text-center">Independencia</p>
                   <p className=" w-1/4 text-center">Entregado</p>
@@ -64,7 +63,7 @@ const page = () => {
                     Detalle
                   </p>
                 </div>
-                <div className="flex py-2 text-[18px] mb-3 items-center">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px] mb-3 items-center">
                   <p className=" w-1/4 text-center">Takiven Perú</p>
                   <p className=" w-1/4 text-center">San Miguel</p>
                   <p className=" w-1/4 text-center">Recibido</p>
@@ -77,21 +76,21 @@ const page = () => {
             </div>
           </div>
 
-          <div className="w-5/12">
+          <div className="lg:w-5/12">
             <div className=" py-4 px-4 text-[#3C4A5B] bg-white rounded-[20px] mt-4">
               <div className=" flex gap-3 ">
                 <Image src={start} alt="" />
-                <h3 className="text-[28px] font-[400]">Productos mas vendidos</h3>
+                <h3 className="xl:text-[28px] lg:text-[26px] font-[400]">Productos mas vendidos</h3>
               </div>
               <div className="">
-                <div className=" flex text-[18px] font-[400] pt-4 ">
+                <div className=" flex xl:text-[18px] lg:text-[15px]  font-[400] pt-4 ">
                   <p className=" w-1/4 text-center">IMG</p>
                   <p className=" w-1/4 text-center">Nombre</p>
                   <p className=" w-1/4 text-center">SKU</p>
                   <p className=" w-1/4 text-center">Vendidos</p>
                 </div>
                 <hr className=" border-1 border-black " />
-                <div className="flex py-2 text-[18px]  items-center">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px]   items-center">
                   <div className=" w-1/4 flex justify-center">
                     <div className="w-[68px] h-[74px] border border-black"></div>
                   </div>
@@ -99,7 +98,7 @@ const page = () => {
                   <p className=" w-1/4 text-center">TK-010001</p>
                   <p className=" w-1/4 text-center">23</p>
                 </div>
-                <div className="flex py-2 text-[18px]  items-center">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px]  items-center">
                   <div className=" w-1/4 flex justify-center">
                     <div className="w-[68px] h-[74px] border border-black"></div>
                   </div>
@@ -107,7 +106,7 @@ const page = () => {
                   <p className=" w-1/4 text-center">TK-010001</p>
                   <p className=" w-1/4 text-center">23</p>
                 </div>
-                <div className="flex py-2 text-[18px]  items-center">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px]   items-center">
                   <div className=" w-1/4 flex justify-center">
                     <div className="w-[68px] h-[74px] border border-black"></div>
                   </div>
@@ -121,17 +120,17 @@ const page = () => {
             <div className=" py-4 px-4 text-[#3C4A5B] bg-white rounded-[20px] mt-4">
               <div className=" flex gap-3 ">
                 <Image src={uservend} alt="" />
-                <h3 className="text-[28px] font-[400] ">Usuarios</h3>
+                <h3 className="xl:text-[28px] lg:text-[26px] font-[400] ">Usuarios</h3>
               </div>
               <div className="">
-                <div className=" flex text-[18px] font-[400] pt-4  ">
+                <div className=" flex xl:text-[18px] lg:text-[15px]  font-[400] pt-4  ">
                   <p className=" w-1/4 text-center">Nombre</p>
                   <p className=" w-1/4 text-center">Email</p>
                   <p className=" w-1/4 text-center">Rol</p>
                   <p className=" w-1/4 text-center">Status</p>
                 </div>
                 <hr className=" border-1 border-black mb-3" />
-                <div className="flex py-2 text-[18px]  items-center mb-3">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px]  items-center mb-3">
                   <p className=" w-1/4 text-center">Paulo Abad</p>
                   <p className=" w-1/4 text-center">paulo@.com</p>
                   <p className=" w-1/4 text-center">Admin</p>
@@ -139,7 +138,7 @@ const page = () => {
                     <p className="w-[20px] h-[20px] bg-[#29E197] rounded-full"></p>
                   </div>
                 </div>
-                <div className="flex py-2 text-[18px]  items-center mb-3">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px]  items-center mb-3">
                   <p className=" w-1/4 text-center">Brajhan Lopez</p>
                   <p className=" w-1/4 text-center">brajhan@.com</p>
                   <p className=" w-1/4 text-center">Editor</p>
@@ -147,7 +146,7 @@ const page = () => {
                     <p className="w-[20px] h-[20px] bg-[#29E197] rounded-full"></p>
                   </div>
                 </div>
-                <div className="flex py-2 text-[18px]  items-center mb-3">
+                <div className="flex py-2 xl:text-[18px] lg:text-[15px]   items-center mb-3">
                   <p className=" w-1/4 text-center">Juan Velazco</p>
                   <p className=" w-1/4 text-center">juan@.com</p>
                   <p className=" w-1/4 text-center">Editor</p>
--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,28 +1,28 @@
 import Image from "next/image";
 import { BiHomeAlt, BiStore, BiSolidTruck, BiUser, BiCog } from "react-icons/bi";
-import { FaTruck} from 'react-icons/fa'
+import { FaTruck } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
     <div className="bg-[#EDF0F1] w-[90px] flex flex-col gap-5 items-center">
-      <div className="text-[40px] text-white bg-[#3C4A5B] w-full flex justify-center rounded-r-[20px] py-2">
+      <div className="text-[40px] text-white bg-[#3C4A5B] w-full flex justify-center rounded-r-[20px] py-2 cursor-pointer">
         <BiHomeAlt />
       </div>
 
-      <div className="text-[40px] text-[#3C4A5B]">
+      <div className="text-[40px] text-[#3C4A5B] cursor-pointer">
         <BiStore />
       </div>
 
-      <div className="text-[40px] text-[#3C4A5B]">
-       <FaTruck/>
+      <div className="text-[40px] text-[#3C4A5B] cursor-pointer">
+        <FaTruck />
       </div>
 
-      <div className="text-[40px] text-[#3C4A5B]">
-        <BiUser/>
+      <div className="text-[40px] text-[#3C4A5B] cursor-pointer">
+        <BiUser />
       </div>
 
-      <div className="text-[40px] text-[#3C4A5B]">
-        <BiCog/>
+      <div className="text-[40px] text-[#3C4A5B] cursor-pointer">
+        <BiCog />
       </div>
     </div>
   );
